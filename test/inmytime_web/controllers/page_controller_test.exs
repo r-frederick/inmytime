2d537d5bfdba3c2564a21f6cd11e19e043e6cc96
Ridge Frederick <ridge.frederick@gmail.com>
2018-05-15 20:32:46 -0500

    Retroactively write tests

--- test/inmytime_web/controllers/page_controller_test.exs
+++ test/inmytime_web/controllers/page_controller_test.exs
@@ -3,6 +3,21 @@ defmodule InmytimeWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "inmyti.me"
+  end
+
+  test "GET /1526432420 returns a timestamp represented in both UTC and CDT", %{conn: conn} do
+    conn = conn
+    |> Map.put(:remote_ip, {192, 227, 139, 106}) # Assign an IP in CDT timezone
+    |> get("/1526432420")
+
+    assert html_response(conn, 200) =~ "05/16/2018 @ 1:00am UTC"
+    assert html_response(conn, 200) =~ "05/15/2018 @ 8:00pm CDT"
+  end
+
+  test "GET /malformedorinvalideinput redirects to the current timestamp", %{conn: conn} do
+    conn = get conn, "/malformedorinvalidinput"
+
+    assert html_response(conn, 302)
   end
 end
