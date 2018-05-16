48b29314ce815cd510b4b7723b97eaacd933fc5f
Ridge Frederick <ridge.frederick@gmail.com>
2018-05-15 20:32:25 -0500

    Geolix configuration for test environment

--- config/test.exs
+++ config/test.exs
@@ -6,5 +6,15 @@ config :inmytime, InmytimeWeb.Endpoint,
   http: [port: 4001],
   server: false
 
+# Geolix Configuration
+config :geolix,
+  databases: [
+    %{
+      id: :city,
+      adapter: Geolix.Adapter.MMDB2,
+      source: "../geoip/GeoLite2-City.mmdb"
+    }
+  ]
+
 # Print only warnings and errors during test
 config :logger, level: :warn
